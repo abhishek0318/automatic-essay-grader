08e1d3de1db2e8ac8681e44256999132a3591a26
Abhishek Sharma <abhisheksharma0318@gmail.com>
2017-09-03 00:19:01 +0530

    Added simple feed forard model

---
+++ models/feed_foward.py
@@ -0,0 +1,24 @@
+from constants import GLOVE_DIR
+from keras.layers import Embedding, Dense, Dropout, Flatten
+from keras.models import Sequential
+import keras.regularizers
+from util import tokenizer, load_embedding_matrix
+
+def get_model(embedding_dimension, essay_length):
+    """
+    Returns compiled model.
+    """
+    vocabulary_size = len(tokenizer.word_index) + 1
+    embedding_matrix = load_embedding_matrix(GLOVE_DIR, embedding_dimension)
+
+    model = Sequential()
+    model.add(Embedding(vocabulary_size, embedding_dimension, weights=[embedding_matrix], input_length=essay_length, trainable=False, mask_zero=False))
+    model.add(Flatten())
+    model.add(Dense(500))
+    model.add(Dropout(0.4))
+    model.add(Dense(500))
+    model.add(Dropout(0.4))
+    model.add(Dense(1, activation='sigmoid', activity_regularizer=keras.regularizers.l2(0.0)))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+
+    return model
